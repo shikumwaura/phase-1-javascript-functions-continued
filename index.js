528e3528ca945e0f620c4ab4a0765229a9aa0752
Wanjiku <wanjikumwaura0304@gmail.com>
2025-06-21 11:53:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun())
+
+        // two
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork())
+
+    // Three 
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
